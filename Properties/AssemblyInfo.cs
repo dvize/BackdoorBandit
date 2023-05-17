db9338980c33b4a68b8eaf727ebabdcd0be0d9ef
dvize <dvize404@gmail.com>
2023-05-16 22:01:43 -0700

    Updated for 3.5.6

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -18,6 +18,6 @@ using VersionChecker;
 [assembly: Guid("8fdfc616-1704-4056-a87e-2783692cc153")]
 
 
-[assembly: AssemblyVersion("1.3.1.0")]
-[assembly: AssemblyFileVersion("1.3.1.0")]
-[assembly: TarkovVersion(22617)]
+[assembly: AssemblyVersion("1.3.2.0")]
+[assembly: AssemblyFileVersion("1.3.2.0")]
+[assembly: TarkovVersion(23043)]
