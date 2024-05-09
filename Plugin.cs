d599fbf8657dcc8f8158886bb5cfd6a954d90e6e
dvize <dvize404@gmail.com>
2024-05-08 23:28:29 -0700

    Update version

--- Plugin.cs
+++ Plugin.cs
@@ -11,7 +11,7 @@ using VersionChecker;
 
 namespace DoorBreach
 {
-    [BepInPlugin("com.dvize.BackdoorBandit", "dvize.BackdoorBandit", "1.8.6")]
+    [BepInPlugin("com.dvize.BackdoorBandit", "dvize.BackdoorBandit", "1.8.7")]
     //[BepInDependency("com.spt-aki.core", "3.7.6")]
     public class DoorBreachPlugin : BaseUnityPlugin
     {
